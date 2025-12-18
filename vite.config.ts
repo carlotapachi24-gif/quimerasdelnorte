67ab09ffc156871befddb7d8bc3d9911ce899b59
carlotapachi24-gif <carlotapachi24@gmail.com>
2025-12-18 20:18:36 +0100

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/the-narrative-archive/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
