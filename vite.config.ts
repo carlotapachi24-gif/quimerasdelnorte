e5397efe04818ca635c153791c5fe0c3f1f0fad3
carlotapachi24-gif <carlotapachi24@gmail.com>
2025-12-19 16:23:08 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/the-narrative-archive/",
+base: "/quimerasdelnorte/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
