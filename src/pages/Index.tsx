06c940b544966cca2c5c002b585e17c3d574da25
carlotapachi24-gif <carlotapachi24@gmail.com>
2025-12-27 15:58:25 +0100

    iu

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,8 @@ const Index = () => {
     <Layout>
       {/* Hero Section */}
       <section className="min-h-screen flex flex-col justify-center relative px-6 pattern-bg pt-24">
+        {/* Hero background illustration */}
+        <div className="absolute inset-0 hero-bg opacity-30 dark:opacity-15 z-0 pointer-events-none" />
         <div className="container mx-auto max-w-5xl relative z-10">
           <div className="space-y-8">
             {/* Decorative element */}
